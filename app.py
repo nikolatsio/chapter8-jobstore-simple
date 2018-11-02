3bc9dd75da598c568067a9396509f26086e751e3
Nikolay Nikolov <nikolay.nikolov@cnsys.bg>
2018-11-02 14:16:28 +0200

    up17 app

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def post_job():
 
 if __name__ == '__main__':
 	# app.run(debug=True)
-        app.run(host='0.0.0.0', debug=True, use_reloader=False)
+        app.run(host='0.0.0.0', debug=True, use_reloader=True)
